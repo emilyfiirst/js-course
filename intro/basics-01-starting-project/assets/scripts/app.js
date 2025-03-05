65a3e3d0b2e79beaf615cdb597c4bcca2c76fdda
emilianyfiirst <emiliany20@live.com>
2025-03-04 22:26:16 -0300

    ajustes

--- intro/basics-01-starting-project/assets/scripts/app.js
+++ intro/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,10 @@
-alert('deu certo')
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(){
+    const calcDescription = `${currentResult} + ${userInput.value}`
+    currentResult = currentResult + parseInt(userInput.value); 
+    outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click', add);
